40064ed55397a9f3a35e493dc68b0d2644c0bc6a
alenasahakyan <alenasahakyan>
2023-03-05 13:20:58 +0400

    final

--- server.js
+++ server.js
@@ -20,18 +20,5 @@ server.listen(3000);
 
 io.on('connection', function (socket) {
 
-    for(var i in messages) {
-    
-    socket.emit("display message", messages[i]);
-    
-    }
-    
-    socket.on("send message", function (data) {
-    
-    messages.push(data);
-    
-    io.sockets.emit("display message", data);
-    
-    });
     
     });
